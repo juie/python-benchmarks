3c8a4e94570597deb46dbbd579a647e695739faa
Leonardo de Oliveira <leodeoliveira.info@gmail.com>
2015-08-11 17:37:14 -0300

    stress py

---
+++ fio.py
@@ -0,0 +1,16 @@
+from math import sqrt
+
+def main():
+    a = sqrt(930128310923810928319028310923812380912831092830912)
+    b = a*102/0.312313123123
+
+    c = sqrt(a+b)
+    print c
+
+
+
+
+
+if __name__ == '__main__':
+    while True:
+        main()
